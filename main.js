86d2d8f4dbc19b0c2fea8c7a6bf53b793dd07e31
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2019-10-17 13:57:03 +0100

    Added images, randomise tiles, and win state

--- main.js
+++ main.js
@@ -1,73 +1,194 @@
-const canvas = document.querySelector(".game-window");
-const ctx = canvas.getContext("2d");
-
 class Puzzle {
-	constructor(width, height, columns, rows) {
-		this.width = width;
-		this.height = height;
-		canvas.setAttribute('width', width);
-		canvas.setAttribute('height', height);
-		canvas.rect = canvas.getBoundingClientRect();
+	constructor({ image = "dali.jpg", columns = 4, rows = 4, emptyX = 0, emptyY = 0 }) {
+		this.image = new Image();
+		this.image.onload = () => {
+			this.width = this.image.width;
+			this.height = this.image.height;
 
-		this.columns = columns;
-		this.rows = rows;
-		this.tileWidth = width / columns;
-		this.tileHeight = height / rows;
-		
-		this.tiles = [];
-		for (let y = 0; y < rows; y++) {
-			this.tiles[y] = [];
-			for (let x = 0; x < columns; x++) {
-				this.tiles[y][x] = (y * columns) + x;
+			this.canvas = document.querySelector(".game-window");
+			this.ctx = this.canvas.getContext("2d");
+			this.canvas.setAttribute('width', this.width);
+			this.canvas.setAttribute('height', this.height);
+			this.canvas.rect = this.canvas.getBoundingClientRect();
+
+			this.columns = columns;
+			this.rows = rows;
+			this.tileWidth = this.width / columns;
+			this.tileHeight = this.height / rows;
+			
+			this.tiles = [];
+			for (let y = 0; y < rows; y++) {
+				for (let x = 0; x < columns; x++) {
+					const tile = {id: (y * columns) + x, x, y, imgX: x, imgY: y};
+					if (x === emptyX && y === emptyY) this.emptyTile = tile;
+					this.tiles.push(tile);
+				}
 			}
+
+			document.addEventListener('keydown', e => this.onKeyDown(e));
+			this.canvas.addEventListener('click', e => this.onClick(e));
+			this.canvas.addEventListener('contextmenu', e => {
+				e.preventDefault();
+				this.onClick(e);
+			});
+
+			this.startGame();
 		}
-		this.selectedTile = {x: 0, y: 0};
-		
-		canvas.addEventListener('click', e => this.onClick(e));
-		this.draw();
+		this.image.src = image;	
 	}
 
 	onClick(e) {
-		const x = Math.floor((e.x - canvas.rect.left) / this.tileWidth);
-		const y = Math.floor((e.y - canvas.rect.top) / this.tileHeight);
-		this.switchTiles(x, y);
+		if (!this.emptyTile) return;
+
+		const x = Math.floor((e.x - this.canvas.rect.left) / this.tileWidth);
+		const y = Math.floor((e.y - this.canvas.rect.top) / this.tileHeight);
+		this.makeMove(x, y);
+	}
+	
+	onKeyDown(e) {
+		if (!this.emptyTile) return;
+
+		let { x, y } = this.emptyTile;
+		if (e.keyCode === 37 || e.keyCode === 65) x++;			// Left or A
+		else if (e.keyCode === 39 || e.keyCode === 68) x--;	// Right or D
+		else if (e.keyCode === 38 || e.keyCode === 87) y++;	// Up or W
+		else if (e.keyCode === 40 || e.keyCode === 83) y--;	// Down or S
+		
+		this.makeMove(x, y);
+	}
+
+	startGame() {
+		// if (this.columns === this.rows) this.randomiseTiles();
+		// else this.randomlyPlay();
+
+		this.randomlyPlay();
+		this.draw();
+	}
+
+	randomiseTiles() {
+		do {
+			for (let i = this.tiles.length - 1; i > 0; i--) {
+				const j = Math.floor(Math.random() * (i + 1));
+				[this.tiles[i], this.tiles[j]] = [this.tiles[j], this.tiles[i]];
+			}
+			for (let y = 0; y < this.rows; y++) {
+				for (let x = 0; x < this.columns; x++) {
+					const tile = this.tiles[y * this.columns + x];
+					tile.x = x;
+					tile.y = y;
+				}
+			}
+		}
+		while (!this.checkIsSolvable());
+	}
+	
+	checkIsSolvable() {
+		let inversions = 0;
+		for (let i = 0; i < this.tiles.length - 1; i++) {
+			for (let j = i + 1; j < this.tiles.length; j++) {
+				if (this.tiles[i].id > this.tiles[j].id) inversions++;
+			}
+		}
+
+		if (this.columns % 2 === 1) {
+			if (inversions % 2 === 0) return true;
+			else return false;
+		}
+		else {
+			if ((this.rows - this.emptyTile.y) % 2 === 0) {
+				if (inversions % 2 === 1) return true;
+				else return false;
+			}
+			else {
+				if (inversions % 2 === 0) return true;
+				else return false;
+			}
+		}
+	}
+
+	randomlyPlay() {
+		for (let i = 0; i < this.columns * this.rows * 100; i++) {
+			switch (Math.floor(Math.random() * 4)) {
+				case 0:
+					if (this.emptyTile.x > 0) this.switchTiles(this.emptyTile.x - 1, this.emptyTile.y);
+				break;
+				case 1:
+					if (this.emptyTile.x < this.columns - 1) this.switchTiles(this.emptyTile.x + 1, this.emptyTile.y);
+				break;
+				case 2:
+					if (this.emptyTile.y > 0) this.switchTiles(this.emptyTile.x, this.emptyTile.y - 1);
+				break;
+				case 3:
+					if (this.emptyTile.y < this.rows - 1) this.switchTiles(this.emptyTile.x, this.emptyTile.y + 1);
+				break;
+			}
+		}
+	}
+
+	makeMove(x, y) {
+		if (x < 0 || x >= this.columns || y < 0 || y >= this.rows) return;
+
+		if (this.emptyTile && this.switchTiles(x, y)) {
+			if (this.checkIsComplete()) this.winGame();
+			this.draw();
+		}
 	}
 
 	switchTiles(x, y) {
-		const x2 = this.selectedTile.x;
-		const y2 = this.selectedTile.y;
-		const distanceX = Math.abs(x - x2);
-		const distanceY = Math.abs(y - y2);
+		const selectedTile = this.tiles.find(tile => tile.x === x && tile.y === y);
+		const distanceX = Math.abs(x - this.emptyTile.x);
+		const distanceY = Math.abs(y - this.emptyTile.y);
 
-		if ((distanceX === 1 && y === y2) || (distanceY === 1 && x === x2)) {
-			const temp = this.tiles[y][x];
-			this.tiles[y][x] = this.tiles[y2][x2];
-			this.tiles[y2][x2] = temp;
-			this.selectedTile = {x, y};
-			this.draw();
+		if ((distanceX === 1 && y === this.emptyTile.y) || (distanceY === 1 && x === this.emptyTile.x)) {
+			selectedTile.x = this.emptyTile.x;
+			selectedTile.y = this.emptyTile.y;
+			this.emptyTile.x = x;
+			this.emptyTile.y = y;
+			return true;
 		}
-		else {
-			console.log("Invalid Move");
+	}
+	
+	checkIsComplete() {
+		for (let i = 0; i < this.tiles.length; i++) {
+			const { id, x, y } = this.tiles[i];
+			if (id !== y * this.columns + x) return false;
 		}
+		return true;
+	}
+
+	winGame() {
+		console.log("You Win!");
+		this.emptyTile = null;
 	}
 
 	draw() {
-		ctx.clearRect(0, 0, this.width, this.height);
-		ctx.font = "30px Arial";
-		for (let y = 0; y < this.rows; y++) {
-			for (let x = 0; x < this.columns; x++) {
-				if (this.tiles[y][x] === 0) ctx.fillStyle = '#000';
-				else ctx.fillStyle = '#00f';
-				ctx.fillRect(x * this.tileWidth, y * this.tileHeight, this.tileWidth, this.tileHeight);
+		this.ctx.clearRect(0, 0, this.width, this.height);
 
-				ctx.strokeRect(x * this.tileWidth, y * this.tileHeight, this.tileWidth, this.tileHeight);
-
-				if (this.tiles[y][x] === 0) ctx.fillStyle = '#000';
-				else ctx.fillStyle = '#ff0';
-				ctx.fillText(this.tiles[y][x], (x * this.tileWidth) + (this.tileWidth / 2), (y * this.tileHeight) + (this.tileHeight / 2));
-			}
+		for (let i = 0; i < this.tiles.length; i++) {
+			const { x, y, imgX, imgY } = this.tiles[i];
+			if (this.emptyTile === this.tiles[i]) this.ctx.fillRect(x * this.tileWidth, y * this.tileHeight, this.tileWidth, this.tileHeight);
+			else this.ctx.drawImage(this.image, this.tileWidth * imgX, this.tileHeight * imgY, this.tileWidth, this.tileHeight, this.tileWidth * x, this.tileHeight * y, this.tileWidth, this.tileHeight);
+			if (this.emptyTile) this.ctx.strokeRect(x * this.tileWidth, y * this.tileHeight, this.tileWidth, this.tileHeight);
 		}
 	}
 }
 
-const slide = new Puzzle(400, 500, 3, 4);
+const config1 = {
+	image: "dali.jpg",
+	columns: 3, rows: 4,
+	emptyX: 2, emptyY: 0,
+};
+
+const config2 = {
+	image: "dali2.jpg",
+	columns: 3, rows: 3,
+	emptyX: 2, emptyY: 0,
+};
+
+const config3 = {
+	image: "dali3.jpg",
+	columns: 4, rows: 4,
+	emptyX: 3, emptyY: 0,
+};
+
+const puzzle = new Puzzle(config3);
